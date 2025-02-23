c898483c1d1d949611bfacc1de761dd192baa3f7
premnathuthirapathi <155531523+premnathuthirapathi@users.noreply.github.com>
2025-02-23 19:22:26 +0530

    Update File.js

--- models/File.js
+++ models/File.js
@@ -2,11 +2,10 @@
 const mongoose = require('mongoose');
 
 const fileSchema = new mongoose.Schema({
-    filename: { type: String, required: true },
-    category: { type: String, required: true },
-    // Add these fields
-    title: { type: String, default: '' },
-    description: { type: String, default: '' }
+  filename: { type: String, required: true },
+  category: { type: String, required: true },
+  title: { type: String, default: '' },
+  description: { type: String, default: '' }
 });
 
 const File = mongoose.model('File', fileSchema);
